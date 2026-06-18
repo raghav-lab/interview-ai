2abfdff604b3de0c268dbe3f3c40c0ab3afd40da
Raghav Chauhan <raghavchauhan@Raghav.local>
2026-06-18 19:24:11 +0530

    Connect FastAPI with PostgreSQL using SQLAlchemy

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 
+from app.database.connection import engine
+from app.database.models import Base
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 @app.get("/")
